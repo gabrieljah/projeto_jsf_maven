fa54fe0a21d4ccd79b30681bdd68185d13a3bdda
Gabriel Araujo Leite <gabrieljah@ovi.com>
2013-08-02 15:24:38 -0300

    Adição da Entidade Pessoa

---
+++ projeto_jsf_maven/src/main/java/br/com/projeto/model/entity/Pessoa.java
@@ -0,0 +1,118 @@
+package br.com.projeto.model.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.*;
+
+/**
+ *
+ * @author gabriel
+ */
+
+@Entity
+@Table(name="pessoa")
+public class Pessoa implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue
+    @Column(name="IdPessoa", nullable=false)
+    private Integer idPessoa;
+    @Column(name="Nome", nullable=false, length=80)
+    private String nome;
+    @Column(name="Email", nullable=false, length=80)
+    private String email;
+    @Column(name="Telefone", nullable=false, length=14)
+    private String telefone;
+    @Column(name="CPF", nullable=false, length=80)
+    private String cpf;
+    @Column(name="Data_de_Nascimento", nullable=false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataDeNascimento;
+    @Column(name="Data_de_Cadastro", nullable=false)
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dataDeCadastro;
+
+    public Pessoa() {
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataDeCadastro() {
+        return dataDeCadastro;
+    }
+
+    public void setDataDeCadastro(Date dataDeCadastro) {
+        this.dataDeCadastro = dataDeCadastro;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(Integer idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pessoa other = (Pessoa) obj;
+        if (this.idPessoa != other.idPessoa && (this.idPessoa == null || !this.idPessoa.equals(other.idPessoa))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + (this.idPessoa != null ? this.idPessoa.hashCode() : 0);
+        return hash;
+    }
+        
+}
---
+++ projeto_jsf_maven/src/main/java/br/com/projeto/util/PhaseListenerProjeto.java
@@ -0,0 +1,51 @@
+package br.com.projeto.util;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author gabriel
+ */
+public class PhaseListenerProjeto implements PhaseListener {
+
+    //Antes da Fase
+    @Override
+    public void beforePhase(PhaseEvent fase) {
+        System.out.println("Antes da Fase: " + getPhaseId());
+        if (fase.getPhaseId().equals(PhaseId.RESTORE_VIEW)) {
+            
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            FacesContextUtil.getRequestSession();
+        }
+    }
+
+    //Depois da Fase
+    @Override
+    public void afterPhase(PhaseEvent fase) {
+        System.out.println("Depois da Fase: " + getPhaseId());
+        if (fase.getPhaseId().equals(PhaseId.RENDER_RESPONSE)) {
+            
+            Session session = FacesContextUtil.getRequestSession();
+            try {
+                session.getTransaction().commit();
+            } catch (Exception e) {
+                if (session.getTransaction().isActive()) {
+                    session.getTransaction().rollback();
+                }
+            }finally{
+                session.close();
+            }
+            
+        }
+        
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.ANY_PHASE;
+    }
+}
